22245ff4c55f5ae134593be87d442afde29688e9
Allzyn <renan.kael@gmail.com>
2026-07-09 10:51:34 -0300

    Side-bar Oculta

--- DevSpace/src/components/Sidebar.jsx
+++ DevSpace/src/components/Sidebar.jsx
@@ -3,35 +3,58 @@ import logo from "../assets/IMGS/Black-DevSpace-removebg-preview.png";
 import paginaInicial from "../assets/IMGS/Home.png";
 import explorar from "../assets/IMGS/Explorar.png";
 import perfil from "../assets/IMGS/NoPerfil.png";
+// import chat from "../assets/IMGS/Chat.png";
 
-export default function Sidebar({ onReload, irPerfil, irExplorar, onOpenPost }) {
+export default function Sidebar({
+  isOpen,
+  onToggle,
+  onReload,
+  irPerfil,
+  irExplorar,
+  onOpenPost,
+  irChat,
+}) {
   return (
-    <div className="sidebar">
-      <div className="logo" onClick={onReload}>
-        <img src={logo} alt="DevSpace Logo" />
+    <>
+      <button
+        className="sidebar-toggle"
+        style={{ left: isOpen ? "260px" : "15px" }}
+        onClick={onToggle}
+      >
+        {isOpen ? "≡" : "≡"}
+      </button>
+
+      <div className={`sidebar ${isOpen ? "open" : "closed"}`}>
+        <div className="logo" onClick={onReload}>
+          <img src={logo} alt="DevSpace Logo" />
+        </div>
+
+        <nav>
+          <div className="nav-item" onClick={onReload}>
+            <img src={paginaInicial} alt="" />
+            <span>Página Inicial</span>
+          </div>
+
+          <div className="nav-item" onClick={irExplorar}>
+            <img src={explorar} alt="" />
+            <span>Explorar</span>
+          </div>
+
+          <div className="nav-item" onClick={irPerfil}>
+            <img src={perfil} alt="" />
+            <span>Perfil</span>
+          </div>
+
+          <div className="nav-item" onClick={irChat}>
+            {/* <img src={chat} alt="" /> */}
+            <span>Chat</span>
+          </div>
+        </nav>
+
+        <button className="post-btn" onClick={onOpenPost}>
+          Postar
+        </button>
       </div>
-
-      <nav>
-        <div className="nav-item" onClick={onReload}>
-          <img src={paginaInicial} />
-          <span>Página Inicial</span>
-        </div>
-
-        <div className="nav-item" onClick={irExplorar}>
-          <img src={explorar} />
-          <span>Explorar</span>
-        </div>
-
-        <div className="nav-item" onClick={irPerfil}>
-          <img src={perfil} />
-          <span>Perfil</span>
-        </div>
-        <div className="nav-item" onClick={irChat}>
-          <img src={} />
-          <span>Chat</span>
-        </div>
-      </nav>
-      <button className="post-btn" onClick={onOpenPost}>Postar</button>
-    </div>
+    </>
   );
 }
